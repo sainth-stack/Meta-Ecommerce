ff88d08e486e93ff42873d779ee45e76627b6597
sainath <123sainathreddy100@gmail.com>
2022-02-12 22:38:52 +0530

    commit

--- index.js
+++ index.js
@@ -13,12 +13,10 @@ app.use(bodyParser.json())
 app.use('/api/products',productsRoute)
 app.use('/api/user',userRoutes)
 app.use('/api/orders',orderRoute)
-if(process.env.NODE_ENV === 'production'){
     app.use(express.static('client/build'))
     app.get('*',(req,res)=>{
         res.sendFile(path.resolve(__dirname,'client','build','index.html'))
     })
-}
 
 const Port=process.env.PORT || 5000;
 app.listen(Port,()=>console.log(`node js server started`))
