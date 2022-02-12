494462354a2121368dc81ef4c877a15c39efa2b6
sainath <123sainathreddy100@gmail.com>
2022-02-12 23:03:08 +0530

    changes commited

--- index.js
+++ index.js
@@ -14,13 +14,12 @@ app.use('/api/products',productsRoute)
 app.use('/api/user',userRoutes)
 app.use('/api/orders',orderRoute)
 if (process.env.NODE_ENV === "production") {
-    app.enable("trust proxy");
   
     //Set static folder
-    app.use(express.static(path.join(__dirname, "/../client/build")));
+    app.use(express.static('/',path.join(__dirname, "/client/build")));
   
     app.get("*", (req, res) => {
-      res.sendFile(path.join(__dirname + "/../client/build/index.html"));
+      res.sendFile(path.resolve(__dirname ,"/client/build/index.html"));
     });
   }
 
